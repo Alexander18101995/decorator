c659f91b71cdf5446de34d2d6141482dcd5a46a5
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-09-01 14:30:50 +0300

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -3,14 +3,13 @@ from datetime import datetime
 l = 'logger.txt'
 
 def dec(func):
-  def logger(a,b):
+  def logger(*args,**kwargs):
     now = datetime.now()
-    res = func(a,b)
+    res = func(*args,**kwargs)
     with open('logger.txt','a', encoding='utf-8') as log:
        log.write(f'Вызов функции {func}, время - {now}\n  с результатом {res}')
   print(f'путь к логам {l}')
   return logger
-
 
 @dec  
 def sum(a,b):

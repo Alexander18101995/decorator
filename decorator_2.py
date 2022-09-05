0d82857d0d2731c674397cbe7be70a8815833954
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-09-05 10:26:39 +0300

    Update decorator_2.py

--- decorator_2.py
+++ decorator_2.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import requests
 
 
-file_path = 'logs.txt'
+path = 'logs.txt'
 
 
 def get_log(path):
@@ -21,7 +21,7 @@ def get_log(path):
     return decor
 
 
-@get_log(file_path)
+@get_log(path)
 def get_status(*args, **kwargs):
     url = ','.join(args)
     response = requests.get(url=url)
